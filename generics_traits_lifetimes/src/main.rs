6913f35f387d34312c4bc34dbf3bee238e62c3eb
dcbuilder <dcbuilder@protonmail.com>
2022-01-05 14:04:40 +0100

    generics + types + lifetimes

--- generics_traits_lifetimes/src/main.rs
+++ generics_traits_lifetimes/src/main.rs
@@ -1,3 +1,20 @@
+use std::fmt::Display;
+
+fn longest_with_an_announcement<'a, T>(x: &'a str, y: &'a str, ann: T) -> &'a str
+where
+    T: Display,
+{
+    println!("Announcement! {}", ann);
+    if x.len() > y.len() {
+        x
+    } else {
+        y
+    }
+}
+struct ImportantExcerpt<'a> {
+    part: &'a str,
+}
+
 fn largest<T: PartialOrd + Copy>(list: &[T]) -> T {
     let mut largest = list[0];
 
@@ -20,4 +37,10 @@ fn main() {
 
     let result = largest(&char_list);
     println!("The largest char is {}", result);
+
+    let novel = String::from("Call me Ishmael. Some years ago...");
+    let first_sentence = novel.split('.').next().expect("Could not find a '.'");
+    let i = ImportantExcerpt {
+        part: first_sentence,
+    };
 }
